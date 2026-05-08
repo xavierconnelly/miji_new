50b21534c0153921964e484a9b1ed0ed8c512137
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2026-05-08 13:51:12 +1000

    bump

---
+++ .svelte-kit/generated/client/app.js
@@ -0,0 +1,28 @@
+export { matchers } from './matchers.js';
+
+export const nodes = [
+	() => import('./nodes/0'),
+	() => import('./nodes/1'),
+	() => import('./nodes/2'),
+	() => import('./nodes/3'),
+	() => import('./nodes/4'),
+	() => import('./nodes/5'),
+	() => import('./nodes/6'),
+	() => import('./nodes/7'),
+	() => import('./nodes/8')
+];
+
+export const server_loads = [];
+
+export const dictionary = {
+		"/": [5],
+		"/about": [6,[2]],
+		"/projects": [~7,[3]],
+		"/projects/[slug]": [~8,[3,4]]
+	};
+
+export const hooks = {
+	handleError: (({ error }) => { console.error(error) }),
+};
+
+export { default as root } from '../root.svelte';
