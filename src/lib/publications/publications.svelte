cc1687f3a97d4b4598cef97d12fdd0bbdb4ccd2d
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2026-05-11 10:34:11 +1000

    publication loop

--- src/lib/publications/publications.svelte
+++ src/lib/publications/publications.svelte
@@ -1,304 +1,194 @@
+<script>
+    import { projects } from '../../routes/projects/projects.js';
+    import { publications } from './publications.js';
+</script>
+
 <div id="publications">
-	<h6> Publications</h6>
-	<!-- <br> -->
-	<div class="row title">
-		<div class="year">Year</div>
-		<div class="publisher">Publisher</div>
-		<div class="issue">Issue</div>
-		<div class="project">Project</div>
-	</div>
- 	
-	<a href="https://architectureau.com/articles/a-light-addition-by-office-miji/" target="blank" data-sveltekit-noscroll class="year2026 row ALightAddition">
-		<div class="year">2026</div>
-		<div class="publisher">Architecture, AU</div>
-		<div class="issue">Online</div>
-		<div class="project">A Light Addition</div>
-	</a>
+    <h6>Publications</h6>
+    <div class="row title">
+        <div class="year">Year</div>
+        <div class="publisher">Publisher</div>
+        <div class="issue">Issue</div>
+        <div class="project">Project</div>
+    </div>
 
-	<a href="https://unionmagazine.com/the-patchwork/" target="blank" data-sveltekit-noscroll class="year2025 row">
-		<div class="year">2025</div>
-		<div class="publisher">Union Magazine</div>
-		<div class="issue">Online</div>
-		<div class="project">Parts of Practice</div>
-	</a>
+    {#each publications as p}
+        {@const project = projects.find(pr => pr.slug === p.project)}
+        {@const href = p.url ?? (project ? `/projects/${project.slug}` : null)}
+        {@const isExternal = !!p.url}
 
-	<a href="https://open.spotify.com/episode/2poJoGmYOG647ZKbUcqckZ?si=W9nrHM49S4GYCNWQR_PPrg" target="blank" data-sveltekit-noscroll class="year2025 row">
-		<div class="year">2025</div>
-		<div class="publisher">Office Dave Sharp Podcast</div>
-		<div class="issue">21 January</div>
-		<div class="project">Office Profile</div>
-	</a>
-	
-	<a href="/projects/ARenovationForThree" data-sveltekit-noscroll class="year2024 row ARenovationForThree">
-		<div class="year">2024</div>
-		<div class="publisher">Houses Magazine</div>
-		<div class="issue">Issue 157</div>
-		<div class="project">A Renovation For Three</div>
-	</a>
-	<span class="ARenovationForThree">
-		<h6>A Renovation For Three</h6>
-		<img src="../images/ARenovationForThree/01_800.webp" alt="A Renovation For Three ">
-	</span>
-	<a href="/projects/ABHouse" data-sveltekit-noscroll class="year2023 row ABHouse">
-		<div class="year">2024</div>
-		<div class="publisher">Revista PLOT</div>
-		<div class="issue">Issue 71</div>
-		<div class="project">Office Profile/AB House</div>
-	</a>
-	<span class="ABHouse">
-		<h6>AB House</h6>
-		<img src="../images/ABHouse/01_800.webp" alt="AB House ">
-	</span>
-	<a href="/projects/ABHouse" data-sveltekit-noscroll class="year2023 row ABHouse">
-		<div class="year">2023</div>
-		<div class="publisher">Grand Designs Australia</div>
-		<div class="issue">Issue 123</div>
-		<div class="project">AB House</div>
-	</a>
-	<span class="ABHouse">
-		<h6>AB House</h6>
-		<img src="../images/ABHouse/01_800.webp" alt="AB House ">
-	</span>
-	<div data-sveltekit-noscroll class="year2023 row">
-		<div class="year">2023</div>
-		<div class="publisher">Architectural Record</div>
-		<div class="issue">Issue 06</div>
-		<div class="project">Office Profile</div>
-	</div>
-	<div data-sveltekit-noscroll class="year2023 row">
-		<div class="year">2023</div>
-		<div class="publisher">Artichoke</div>
-		<div class="issue">Issue 82</div>
-		<div class="project">Office Profile</div>
-	</div>
-	<a href="/projects/ABHouse" data-sveltekit-noscroll class="year2023 row ABHouse">
-		<div class="year">2023</div>
-		<div class="publisher">Houses Magazine</div>
-		<div class="issue">Issue 149</div>
-		<div class="project">AB House</div>
-	</a>
-	<span class="ABHouse">
-		<h6>AB House</h6>
-		<img src="../images/ABHouse/01_800.webp" alt="AB House ">
-	</span>
-	<a href="/projects/ABHouse" data-sveltekit-noscroll class="year2023 row ABHouse">
-		<div class="year">2023</div>
-		<div class="publisher">Domus Web</div>
-		<div class="issue"></div>
-		<div class="project">AB House</div>
-	</a>
-	<span class="ABHouse">
-		<h6>AB House</h6>
-		<img src="../images/ABHouse/01_800.webp" alt="AB House ">
-	</span>
-	<a href="/projects/ABHouse" data-sveltekit-noscroll class="year2023 row ABHouse">
-		<div class="year">2022</div>
-		<div class="publisher">I’m Home</div>
-		<div class="issue">Issue 120</div>
-		<div class="project">AB House</div>
-	</a>
-	<span class="ABHouse">
-		<h6>AB House</h6>
-		<img src="../images/ABHouse/01_800.webp" alt="AB House ">
-	</span>
-	<a href="/projects/ABHouse" data-sveltekit-noscroll class="year2023 row ABHouse">
-		<div class="year">2022</div>
-		<div class="publisher">Divisare</div>
-		<div class="issue"></div>
-		<div class="project">AB House</div>
-	</a>
-	<span class="ABHouse">
-		<h6>AB House</h6>
-		<img src="../images/ABHouse/01_800.webp" alt="AB House">
-	</span>
-	<a href="/projects/AHouseForOne" data-sveltekit-noscroll class="year2023 row AHouseForOne">
-		<div class="year">2022</div>
-		<div class="publisher">Divisare</div>
-		<div class="issue"></div>
-		<div class="project">A House For One</div>
-	</a>
-	<span class="AHouseForOne">
-		<h6>A House For One</h6>
-		<img src="../images/AHouseForOne/01_800.webp" alt="A House For One">
-	</span>
-	<a href="/projects/APavilion" data-sveltekit-noscroll class="year2023 row APavilion">
-		<div class="year">2021</div>
-		<div class="publisher">The Commercial Project</div>
-		<div class="issue">Issue 01</div>
-		<div class="project">A Pavilion</div>
-	</a>
-	<span class="APavilion">
-		<h6>A Pavilion</h6>
-		<img src="../images/APavilion/01_800.webp" alt="AB House">
-	</span>
-	<a href="/projects/APavilion" data-sveltekit-noscroll class="year2023 row APavilion">
-		<div class="year">2021</div>
-		<div class="publisher">Designboom</div>
-		<div class="issue"></div>
-		<div class="project">A Pavilion</div>
-	</a>
-	<span class="APavilion">
-		<h6>A Pavilion</h6>
-		<img src="../images/APavilion/01_800.webp" alt="AB House ">
-	</span>
+        <svelte:element
+            this={href ? 'a' : 'div'}
+            href={href}
+            target={isExternal ? '_blank' : undefined}
+            rel={isExternal ? 'noopener noreferrer' : undefined}
+            data-sveltekit-noscroll
+            class="row dot {project?.slug ?? ''}"
+        >
+            <div class="year">{p.year}</div>
+            <div class="publisher">{p.publisher}</div>
+            <div class="issue">{p.issue ?? ''}</div>
+            <div class="project">{p.label ?? project?.title ?? p.project}</div>
+        </svelte:element>
+
+        {#if project}
+            <span class={project.slug}>
+                <h6>{p.label ?? project.title}</h6>
+                <img src="../images/{project.slug}/{project.images[0]}_800.webp" alt={project.title}>
+            </span>
+        {/if}
+    {/each}
 </div>
 
 <style>
+    h6 {
+        padding-bottom: 1em;
+        border-bottom: 1px solid;
+        margin-bottom: 1em;
+    }
 
-h6 {
-	padding-bottom: 1em; 
-	border-bottom: 1px solid;
-	margin-bottom: 1em
-}	
+    #publications {
+        padding-bottom: 100px;
+        width: calc(100% - 0px);
+        float: right;
+        padding-top: 60px;
+    }
 
-#publications {
-	padding-bottom: 100px;
-	width: calc(100% - 0px);
-	float: right;
-	padding-top: 60px;
-}
+    .title {
+        padding-bottom: 1em;
+    }
 
-.title {
-	padding-bottom: 1em;
-}
+    .row {
+        display: flex;
+        width: 100%;
+        line-height: 130%;
+    }
 
-.row {
-	display: flex;
-	width: 100%;
-	line-height: 130%;
-}
+    .row div {
+        border: none;
+        padding-right: 30px;
+        vertical-align: top;
+        text-align: left;
+    }
 
-.row div {
-	border: none;
-	padding-right: 30px;
-	vertical-align: top;
-	text-align: left;
-}
+    a:hover div { color: var(--green) }
+    .ALightAddition:hover * { color: var(--red) }
+    .ACourtyardHouse:hover * { color: var(--pink) }
+    .ABHouse:hover * { color: var(--pistachio) }
+    .APavilion:hover * { color: var(--yellow) }
+    .ARenovationForThree:hover * { color: var(--green) }
+    .ACertainKindofLife:hover * { color: var(--pink) }
+    .AHouseForOne:hover * { color: var(--green) }
+    .FourVisitsTo52Posts:hover * { color: var(--grey) }
+    .TokyoTina:hover * { color: var(--red) }
 
-a:hover div {color: var(--green)}
-.ALightAddition:hover * {color: var(--red)}
-/* .ACourtyardHouse:hover * {color: var(--pink)} */
-.ABHouse:hover * {color: var(--pistachio)}
-.APavilion:hover * {color: var(--yellow)}
+    .year {
+        width: 10%;
+        padding-right: 60px;
+    }
+    .issue {
+        width: 30%
+    }
+    .publisher {
+        width: 30%;
+    }
+    .project {
+        width: 30%
+    }
 
+    span {
+        width: calc(25% - 40px);
+        position: fixed;
+        left: 0px;
+        top: 30px;
+        padding: 0px 0px 10px 10px;
+        margin-top: 22px;
+        margin-right: 50px;
+        opacity: 0;
+        transition: 2s;
+        background: white;
+        z-index: -99;
+        height: 100%;
+    }
 
-.year {
-	width: 10%;
-	padding-right: 60px;
-}
-.issue {
-	width: 30%
-}
-.publisher {
-	width: 30%;
-}
-.project {
-	width: 30%
-}
+    a:hover + span {
+        transition: 0.5s;
+        opacity: 1;
+        z-index: 99;
+    }
 
-span {
-	width: calc(25% - 40px);
-	position: fixed;
-	left: 0px;
-	top: 30px;
-	padding: 0px 0px 10px 10px;
-	margin-top: 22px;
-	margin-right: 50px;
-	/* display: none; */
-	opacity: 0;
-	transition: 2s;
-	background: white;
-	z-index: -99;
-	height: 100%;
-	/* user-select: none; */
-}
+    a + span img {
+        width: 80%;
+        margin: 0 auto;
+        opacity: 0;
+        transition: 2s;
+    }
+    a:hover + span img {
+        opacity: 1;
+        transition: 2s;
+    }
 
-a:hover + span {
-	/* display: block; */
-	transition: 0.5s;
-	opacity: 1;
-	z-index: 99;
-}
+    #publications .dot::before {
+        content: '';
+        display: inline-block;
+        width: 14px;
+        height: 14px;
+        position: relative;
+        left: 0px;
+        top: 1.5px;
+        vertical-align: middle;
+        -moz-border-radius: 12px;
+        -webkit-border-radius: 12px;
+        border-radius: 12px;
+        border: 0px white solid;
+        margin-right: 10px;
+        margin-left: -24px;
+    }
 
-a + span img {
-	width: 80%;
-	/* padding-top: 1em; */
-	margin: 0 auto;
-	opacity: 0;
-	transition: 2s;
+    #publications .dot:hover::before { background: var(--green) }
+    #publications .ALightAddition:hover::before { background: var(--red) }
+    #publications .ACourtyardHouse:hover::before { background: var(--pink) }
+    #publications .ABHouse:hover::before { background: var(--pistachio) }
+    #publications .APavilion:hover::before { background: var(--yellow) }
+    #publications .ARenovationForThree:hover::before { background: var(--green) }
+    #publications .ACertainKindofLife:hover::before { background: var(--pink) }
+    #publications .AHouseForOne:hover::before { background: var(--green) }
+    #publications .FourVisitsTo52Posts:hover::before { background: var(--grey) }
+    #publications .TokyoTina:hover::before { background: var(--red) }
 
-}
-a:hover + span img {
-	opacity: 1;
-	transition: 2s;
-}
-
-#publications a::before,
-.title::before,
-.year2023::before {
-	content: '';
-	display: inline-block;
-	width: 14px;
-	height: 14px;
-	position: relative;
-	left: 0px;
-    top: 1.5px;
-    vertical-align: middle;
-	-moz-border-radius: px;
-	-webkit-border-radius: 12px;
-	border-radius: 12px;
-	border: 0px white solid;
-	margin-right: 10px;
-    margin-left: -24px;
-}
-
-#publications a:hover::before, #publications .year2023:hover::before  {background: var(--green)}
-#publications .ALightAddition:hover::before {background: var(--red)}
-/* #publications .ACourtyardHouse:hover::before {background: var(--pink)} */
-#publications .ABHouse:hover::before {background: var(--pistachio)}
-/* #publications .ACertainKindofLife:hover::before {background: var(--pink)} */
-/* #publications .AHouseForOne:hover::before {background: var(--green)} */
-#publications .APavilion:hover::before {background: var(--yellow)}
-/* #publications .ARenovationForThree:hover::before {background: var(--green)} */
-/* #publications .FourVisitsTo52Posts:hover::before {background: var(--grey)} */
-/* #publications .TokyoTina:hover::before {background: var(--red)} */
-
-
-@media only screen and (max-width: 800px) {
-
-	#publications a::before,
-	.title::before,
-	.year2023::before  {
-		display: none;
-	}
-	a:hover + span {
-		opacity: 0;
-	}
-	span {
-		display: none;
-	}
-	span img {
-		display: none;
-	}
-	.year {
-		width: 20%;
-	}
-	.issue {
-		display: none;
-	}
-	.publisher {
-		width: 40%
-	}
-	.project {
-		width: 40%
-	}
-	.row {
-		border-top: 1px solid;
-		padding: 10px 0;
-	}
-	h6 {
-		border-bottom: 0px solid;
-		margin-bottom: unset;
-	}	
-}
+    @media only screen and (max-width: 800px) {
+        #publications .dot::before,
+        .title::before {
+            display: none;
+        }
+        a:hover + span {
+            opacity: 0;
+        }
+        span {
+            display: none;
+        }
+        span img {
+            display: none;
+        }
+        .year {
+            width: 20%;
+        }
+        .issue {
+            display: none;
+        }
+        .publisher {
+            width: 40%
+        }
+        .project {
+            width: 40%
+        }
+        .row {
+            border-top: 1px solid;
+            padding: 10px 0;
+        }
+        h6 {
+            border-bottom: 0px solid;
+            margin-bottom: unset;
+        }
+    }
 </style>
